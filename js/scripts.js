1a32f1b64812fff088975443b997ae62630e2e7c
Vadim Hryn <gryn.vadim@gmail.com>
2021-09-08 22:17:27 -0400

    added a loop with the output of Pokemon and with additional verification

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-  {name: 'Charmander', height: 6, types: ['fire']},
-  {name: 'Squirtle', height: 5, types: ['water']}
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Charmander', height: 0.6, types: ['fire']},
+  {name: 'Squirtle', height: 0.5, types: ['water']}
 ];
-console.log(pokemonList);
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+  if(pokemonList[i].height >=0.7){
+    document.write(` - Wow, that’s big!`)
+  }
+  document.write(`<br/>`);
+}
