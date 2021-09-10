6f7a32506a40cfe34dbe041b49f82f1c9ac87a6e
Vadim Hryn <gryn.vadim@gmail.com>
2021-09-09 20:46:46 -0400

    array output using foreach and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Charmander', height: 0.6, types: ['fire']},
-  {name: 'Squirtle', height: 0.5, types: ['water']}
-];
-for (var i = 0; i < pokemonList.length; i++) {
-  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
-  if(pokemonList[i].height >=0.7){
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']}
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+pokemonRepository.add({name: 'Pikachu', height: 0.4, types: ['electric']});
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + ' (height: ' + pokemon.height + ' m.)');
+  if(pokemon.height >=0.7){
     document.write(` - Wow, that’s big!`)
   }
   document.write(`<br/>`);
-}
+});
