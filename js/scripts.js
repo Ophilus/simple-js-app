fe2656d33ad24f99b97cfa0a2982d71692c6ba25
Vadim Hryn <gryn.vadim@gmail.com>
2021-09-10 22:48:54 -0400

    edited add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Squirtle', height: 0.5, types: ['water']}
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['water']
+    }
   ];
+  let pokemonKeyList = Object.keys(pokemonList[0]).sort();
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (typeof pokemon == 'object') {
+      if (JSON.stringify(Object.keys(pokemon).sort()) == JSON.stringify(pokemonKeyList)) {
+        pokemonList.push(pokemon);
+      }
+    }
   }
 
   function getAll() {
@@ -18,10 +34,14 @@ let pokemonRepository = (function () {
     getAll: getAll
   };
 })();
-pokemonRepository.add({name: 'Pikachu', height: 0.4, types: ['electric']});
+pokemonRepository.add({
+  name: 'Pikachu',
+  height: 0.4,
+  types: ['electric']
+});
 pokemonRepository.getAll().forEach(function(pokemon) {
   document.write(pokemon.name + ' (height: ' + pokemon.height + ' m.)');
-  if(pokemon.height >=0.7){
+  if (pokemon.height >= 0.7) {
     document.write(` - Wow, that’s big!`)
   }
   document.write(`<br/>`);
